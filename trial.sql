377bffcf6e39c979906306ab69f19427713b729c
Zainabu Muti <zmuti@strathmore.edu>
2024-09-05 18:15:12 +0300

    Create trial.sql

---
+++ trial.sql
@@ -0,0 +1,9 @@
+create table cars(brand VARCHAR(255), year INT, carNO INT, primary key(carNo));
+
+insert into cars(brand, year, carNo)
+VALUES ('Toyota', 2020, 001);
+
+SELECT * from cars;
+
+insert into cars(brand, year, carNo)
+VALUES ('Ford',001);
